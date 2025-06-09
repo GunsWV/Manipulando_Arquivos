2f96de56a0ce0f5601480e87a46de5c2550c239c
GunsWV <guguwissenbach@hotmail.com>
2025-06-09 17:28:34 +0000

    cadastro_de_pessoa.py

--- Manipulação de Arquivos/atividade_manipulação_de_arquivos/pessoa.py
+++ Manipulação de Arquivos/atividade_manipulação_de_arquivos/pessoa.py
@@ -26,8 +26,4 @@ while True:
     if final == "voltar":
         break
 
-#nome = input("Digite o nome:")
-#email = input("Digite o e-mail:")
-#arquivo = open("pessoa.txt", "a")
-#arquivo.write(nome + " | " + email + "\n")
-#arquivo.close()         
+         
